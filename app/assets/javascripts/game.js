24d464265639920d4812a3b501992b7fb83aee69
Sophie Deziel <sdeziel@acquisio.com>
2015-09-13 14:01:52 -0400

    basic frame rendering and gravity

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,19 +1,64 @@
 function ActionCableGame (params) {
-  this.players_list = new PlayersList()
+  this.players_list = new PlayersList();
+  this.canvas = document.getElementById('field'),
+  this.context = this.canvas.getContext('2d');
+  Position.prototype.default_x = $('#game').width() / 2;
+  Position.prototype.default_y = $('#game').height() / 2;
+  var game = this;
+
+  window.addEventListener('resize', function(){game.resizeCanvas(game)}, false);
+
+  this.createFrame = function(game_instance){
+    var game = game_instance;
+    $.each(game.players_list.get_players(), function(i, player) {
+      game.applyGravity(player.position);
+    });
+    game.drawStuff();
+    window.requestAnimationFrame(function() {
+      game.createFrame(game)
+    });
+  }
+
+  this.drawStuff = function() {
+    var game = this;
+    $.each(game.players_list.get_players(), function(i, player) {
+      var img = new Image();
+      img.onload = function() {
+        game.context.clearRect(player.position.x,  player.position.y, 75, 75);
+        game.context.drawImage(img, player.position.x, player.position.y, 75, 75);
+      };
+      img.src = '/assets/moving_player.gif'
+    });
+  }
+
+  this.applyGravity = function(position) {
+    if (position.y <= this.canvas.height - 65) {
+      position.y = position.y + 2;
+    }
+  }
+
+  this.resizeCanvas = function(game_instance) {
+    this.canvas.width = $('#game').innerWidth();
+    this.canvas.height = $('#game').innerHeight() - $('#players').height();
+    this.drawStuff();
+  }
+
+
+  this.createFrame(this);
 }
 
 ActionCableGame.prototype.update_players = function(players) {
   var game = this;
-    $.each(players.diff(game.players_list.names()), function(i, player_name) {
-      var player = game.players_list.find(player_name)
-        if(player == null) {
-          player = game.players_list.push(player_name);
-        }
-    });
-    $.each(game.players_list.names().diff(players), function(i, player_name) {
-      var player = game.players_list.find(player_name)
+  $.each(players.diff(game.players_list.names()), function(i, player_name) {
+    var player = game.players_list.find(player_name)
+      if(player == null) {
+        player = game.players_list.push(player_name);
+      }
+  });
+  $.each(game.players_list.names().diff(players), function(i, player_name) {
+    var player = game.players_list.find(player_name)
       game.players_list.remove(player_name);
-    });
+  });
 
 }
 
@@ -21,6 +66,7 @@ function Player(name, color) {
   this.name = name;
   this.score = 0;
   this.color = color;
+  this.position = new Position(Position.prototype.default_x, Position.prototype.default_y)
 }
 
 function PlayersList() {
@@ -28,6 +74,10 @@ function PlayersList() {
   var players = []
   $('#game').append($('<div id="players">'));
   $('#players').append($('<ul id="players-list">'));
+
+  this.get_players = function(){
+    return players;
+  }
 
   this.names = function() {
     var array = []
@@ -65,17 +115,21 @@ function PlayersList() {
             .append($('<span class="score">').html(player.score))
             );
     });
+    Game.resizeCanvas();
 
   }
 
 
 }
 
+function Position(x, y) {
+  this.x = (x === undefined) ? Position.prototype.default_x : x;
+  this.y = (y === undefined) ? Position.prototype.default_y : y;
+}
+
 Array.prototype.diff = function(a) {
-    return this.filter(function(i) {return a.indexOf(i) < 0;});
+  return this.filter(function(i) {return a.indexOf(i) < 0;});
 };
-
-
 
 Array.prototype.equals = function (array) {
   // if the other array is a falsy value, return
