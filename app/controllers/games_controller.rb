f32201fa30f1e8d3639c1f789894eafae4bcc85b
Sophie Deziel <sdeziel@acquisio.com>
2015-09-14 01:33:25 -0400

    allow iframe

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,13 @@
 class GamesController < ApplicationController
+  after_action :allow_iframe, only: :show
+
   def show
     cookies.signed[:game_monitor] = true
+  end
+
+private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
   end
 end
